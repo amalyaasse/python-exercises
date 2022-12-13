151c2f889e3daa338ebdaba5b70cfe9b6097773e
Amalya Asse <amaliksrtik@gmail.com>
2022-12-13 17:55:15 +0100

    Add files via upload
    verbesserte Version

--- L5_4_2_wetterstation.py
+++ L5_4_2_wetterstation.py
@@ -4,25 +4,36 @@ Temperatur= []
 
 for i in range(len(Wochentage)):
     print(Wochentage[i])
-    Temp=int(input("Die Temperatur:"))
+    Temp=float(input("Die Temperatur:"))
     Temperatur.append(Temp)
 
 #Deklaration und initialiezierung den Variabeln
 maxT = Temperatur[0]
 minT = Temperatur[0]
-Temp = Temperatur[0] #Durchschnittliche Temperatur
+summe = 0 #Durchschnittliche Temperatur
 
 #Höhste Temperatur
-if Temperatur[i] > maxT:
+for i in range(len(Temperatur)):
+    if Temperatur[i] > maxT:
        maxT = Temperatur[i]
 
 #niedrigste Temperatur
-if Temperatur[i] < minT:
-    minT = Temperatur[i]
+for i in range(len(Temperatur)):
+    if Temperatur[i] < minT:
+        minT = Temperatur[i]
     
+#durchschnittliche Temperatur
+    
+for i in range(len(Temperatur)):
+    summe = summe + Temperatur[i]
+
+MittelTemp = summe/len(Temperatur)
+
+
 
 print("Höhste Temperatur: ", maxT)
 print("Niedrigste Temperatur: ",minT)
 print("Temperaturunterschied: ", maxT-minT)
+print("Durchschnittliche Temperatur: ", MittelTemp)
 
     
